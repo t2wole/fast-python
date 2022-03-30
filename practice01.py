d2ba1397960311617801353679c4a2e641940a83
t2wole <t2wole@gmail.com>
2022-03-30 21:54:26 +0900

    first

---
+++ practice01.py
@@ -0,0 +1,17 @@
+# 입.출력 모두 utf-8
+import sys
+
+print(sys.stdin.encoding)
+print(sys.stdout.encoding)
+
+# 출력문
+print('My name is Nana')
+
+# 변수 선언
+myName = 'Nana'
+
+# 조건문
+if myName == 'Nana':
+    print('Good')
+else:
+    print('Bad')
